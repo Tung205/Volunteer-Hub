01564b005b9c99d16e4bb97bfbbd6966115dbf76
toan_nd <thanthanhteuga@gmail.com>
2025-11-04 00:56:16 +0700

    add Login and Register router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import IntroPage from './pages/IntroPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from "./pages/RegisterPage.jsx";
 import EventPage from "./pages/EventPage.jsx";
 
@@ -8,6 +9,7 @@ export default function App() {
   return (
       <Routes>
           <Route path="/" element={<IntroPage/>} />
+          <Route path="/login" element={<LoginPage/>} />
           <Route path="/register" element={<RegisterPage/>} />
           <Route path="/event" element={<EventPage/>} />
       </Routes>
