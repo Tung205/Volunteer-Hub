3f58c06e799a5724388714f383d7f83f513cc231
toan_nd <thanthanhteuga@gmail.com>
2025-11-06 01:38:13 +0700

    package.json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 import IntroPage from './pages/IntroPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from "./pages/RegisterPage.jsx";
 import EventPage from "./pages/EventPage.jsx";
+import DashBoard from "./pages/DashBoard.jsx";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/login" element={<LoginPage/>} />
           <Route path="/register" element={<RegisterPage/>} />
           <Route path="/event" element={<EventPage/>} />
+          <Route path="/dashboard" element={<DashBoard/>} />
       </Routes>
   )
 }
