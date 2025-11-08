94512339b6b83b68e52ed320da2347b775752495
dungminz <163257469+dungminz@users.noreply.github.com>
2025-11-08 16:29:05 +0700

    add login

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller.js';
 
-const r = Router();
-r.post('/register', AuthController.register);
-export default r;
+const router = Router();
+
+router.post('/register', AuthController.register);
+router.post('/login', AuthController.login)
+
+export default router;
 
--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -10,6 +10,32 @@ export const AuthService = {
     const passwordHash = await bcrypt.hash(password, 10);
     const doc = await User.create({ email, passwordHash, name, roles: ['VOLUNTEER'] });
     return { id: doc._id.toString(), email: doc.email, name: doc.name };
+  },
+
+  async login({ email, password }) {
+    // tìm user theo email
+    const user = await User.findOne({ email });
+    if (!user) {
+      const err = new Error('INVALID_CREDENTIALS'); err.status = 401; throw err;
+    }
+
+    // kiểm tra password
+    const isValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isValid) {
+      const err = new Error('INVALID_CREDENTIALS'); err.status = 401; throw err;
+    }
+
+    // kiểm tra status
+    if (user.status !== 'ACTIVE') {
+      const err = new Error('ACCOUNT_INACTIVE'); err.status = 403; throw err;
+    }
+
+    return {
+      id: user._id.toString(),
+      email: user.email,
+      name: user.name,
+      roles: user.roles
+    };
   }
 };
 
