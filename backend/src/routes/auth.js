56c208147337ddfd639e9c045011c6ebeaa4241c
dungminz <163257469+dungminz@users.noreply.github.com>
2025-11-24 01:28:16 +0700

    fix middleware

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,16 +1,10 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.js';
-import { isAuthenticated } from '../middlewares/index.js';
 
 const router = Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
-
-// Route test middleware
-router.get('/test', isAuthenticated, (req, res) => {
-  res.json({ message: 'Middleware hoạt động!', user: req.user });
-});
 
 export default router;
 
