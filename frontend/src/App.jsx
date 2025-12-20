c659993c218e8aa14bc0fd6f6ae93d32005c490c
toan_nd <thanthanhteuga@gmail.com>
2025-12-20 21:42:48 +0700

    thêm route cho group

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout.jsx';
 import HomePage from './pages/HomePage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from "./pages/RegisterPage.jsx";
 import EventPage from "./pages/EventPage.jsx";
 import DashBoard from "./pages/DashBoard.jsx";
+import GroupPage from "./pages/GroupPage.jsx";
 
 export default function App() {
   return (
-      <Routes>
-          <Route path='/' element={<MainLayout/>}>
-            <Route index element={<HomePage/>} />
-            <Route path="/events" element={<EventPage/>} />
-            <Route path="/dashboard" element={<DashBoard/>} />
-          </Route>
+    <Routes>
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/events" element={<EventPage />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/group/:eventId" element={<GroupPage />} />
+      </Route>
 
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/register" element={<RegisterPage/>} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
-      </Routes>
+    </Routes>
   )
 }
